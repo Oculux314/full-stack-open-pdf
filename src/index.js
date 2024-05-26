444eee07132096d2ff8fd992d7ba4741d85163b1
Oculux <nate.williamson.nz@gmail.com>
2024-05-26 16:55:04 +1200

    :bricks: successfully create pdf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import pdf from "html-pdf";
 import puppeteer from "puppeteer";
 
 let browser;
@@ -9,18 +10,25 @@ async function generatePDF(url, pdfPath) {
   await page.setViewport({ width: 1920, height: 1080 });
   console.log("Navigating to URL: ", url);
   await page.goto(url, { waitUntil: "networkidle0" });
-  console.log("Creating PDF...");
+  console.log("Page loaded successfully.")
   const html = await page
     .locator("#course-contents")
     .map((elem) => elem.childNodes[0].childNodes[0].innerHTML)
     .wait();
   console.log(html);
+  console.log("Creating PDF...");
   // await page.pdf({ path: pdfPath, format: "a4" });
+  pdf.create(html).toFile(pdfPath, (err, res) => {
+    if (err) {
+      console.log("Error creating PDF: ", err);
+    }
+    console.log("PDF created successfully.");
+  });
   console.log("PDF created successfully.");
   await browser.close();
 }
 
-generatePDF("https://www.google.com/", "test.pdf")
+generatePDF("https://fullstackopen.com/en/", "test.pdf")
   .then(() => {
     console.log("Successfully generated PDF.");
   })
