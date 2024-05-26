d6d5079fa049d3880c261e7254195506c54937e4
Oculux <nate.williamson.nz@gmail.com>
2024-05-27 01:54:48 +1200

    :bug: make highlighted code render correctly

--- src/html.js
+++ src/html.js
@@ -22,7 +22,8 @@ export function buildHtmlDoc(websiteContent) {
 
   pre {
     color: #fff;
-    background-color: #333;
+    font-weight: bold;
+    background-color: #000;
     padding: 10px;
     border-radius: 5px;
     font-family: consolas, monospace;
@@ -38,68 +39,77 @@ export function buildHtmlDoc(websiteContent) {
   }
 
   :not(pre)>code[class*=language-],pre[class*=language-] {
-      background: #4d4033
+    background: #4d4033
   }
 
   pre[class*=language-] {
-      padding: 1em;
-      margin: .5em 0;
-      overflow: auto;
-      border: .3em solid #7a6652;
-      border-radius: .5em;
-      box-shadow: inset 1px 1px .5em #000
+    padding: 1em;
+    margin: .5em 0;
+    overflow: auto;
+    border: .3em solid #7a6652;
+    border-radius: .5em;
+    box-shadow: inset 1px 1px .5em #000
   }
 
   :not(pre)>code[class*=language-] {
-      padding: .15em .2em .05em;
-      border-radius: .3em;
-      border: .13em solid #7a6652;
-      box-shadow: inset 1px 1px .3em -.1em #000;
-      white-space: normal
+    padding: .15em .2em .05em;
+    border-radius: .3em;
+    border: .13em solid #7a6652;
+    box-shadow: inset 1px 1px .3em -.1em #000;
+    white-space: normal
   }
 
   .token.cdata,.token.comment,.token.doctype,.token.prolog {
-      color: #998066
+    color: #998066
   }
 
   .token.namespace,.token.punctuation {
-      opacity: .7
+    opacity: .7
   }
 
   .token.boolean,.token.constant,.token.number,.token.property,.token.symbol,.token.tag {
-      color: #d1949e
+    color: #d1949e
   }
 
   .token.attr-name,.token.builtin,.token.char,.token.inserted,.token.selector,.token.string {
-      color: #bde052
+    color: #bde052
   }
 
   .language-css .token.string,.style .token.string,.token.entity,.token.operator,.token.url,.token.variable {
-      color: #f5b83d
+    color: #f5b83d
   }
 
   .token.atrule,.token.attr-value,.token.keyword {
-      color: #d1949e
+    color: #d1949e
   }
 
   .token.important,.token.regex {
-      color: #e90
+    color: #e90
   }
 
   .token.bold,.token.important {
-      font-weight: 700
+    font-weight: 700
   }
 
   .token.italic {
-      font-style: italic
+    font-style: italic
   }
 
   .token.entity {
-      cursor: help
+    cursor: help
   }
 
   .token.deleted {
-      color: red
+    color: red
+  }
+
+  .gatsby-highlight-code-line {
+    background-color: hsla(0,0%,88.2%,.3);
+    display: block;
+    margin-right: -1em;
+    margin-left: -1em;
+    padding-right: 1em;
+    padding-left: 1em
   }
   `;
 
